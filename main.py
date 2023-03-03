37d2eb995ff80f7d2fea973fb9f3f87a527249f9
Geraldo Arvelo <119348946+GArvelo@users.noreply.github.com>
2023-03-03 20:35:12 +0000

    Day 2

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ print("Welcome to the tip calculator.")
 
 #2 Create 3 var equal to.
 total_bill = input("What was the total bill? ")
-percentage = input(
-    "What percentage tip would you like to give? 15, 18, or 20? ")
+percentage = input("What percentage tip would you like to give? 15, 18, or 20? ")
 people = input("How many people to split the bill? ")
 
 #3 Changes those var into int and float.
